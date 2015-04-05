0e3b9f9abfb897f5fc9441e50f54e3b6631f4269
Eltha Black <eltha@yandex.ru>
2015-04-05 03:32:25 +0300

    added the whole world trigger (whatever it is) loader from /dev/null

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3.4
 
 import sys
+import os
 import argparse
-import emonoda
+import feedparser
 
 from emonoda import tfile
 
 from emonoda.apps import init
 from emonoda.apps import get_configured_log
 from emonoda.apps import get_configured_client
-from emonoda import tfile
 from emonoda.plugins.fetchers import read_url, build_opener
 
 
-torrent_url = "http://www.nyaa.se/?page=download&tid=674041"
-dest_path = "/media/Elements/Anime"
+#torrent_url = "http://www.nyaa.se/?page=download&tid=674041"
+rss_url = "http://www.nyaa.se/?page=rss&term=%5BHorribleSubs%5D+World+Trigger+720"
+dest_prefix = "/media/Elements/Anime/"
+anime_name = "World Trigger"
 
+dest_path = dest_prefix + anime_name
 
 def main():
     (parent_parser, argv, config) = init()
@@ -36,9 +39,17 @@ def main():
                 log=log_stderr,
             )
             log_stdout.print("Torrent client initialised: %s", (client,))
-            t = tfile.Torrent(data=read_url(build_opener(), torrent_url), path='/dev/null')
-            client.load_torrent(t, dest_path)
-            log_stdout.print("Torrent loaded to %s", dest_path)
+            hashes = client.get_hashes()
+            feed = feedparser.parse(rss_url)
+            log_stdout.print("Loaded %s feed", feed['feed']['title'])
+            os.makedirs(dest_path, exist_ok=True)
+            for entry in feed['entries']:
+                torrent_url = entry['link']
+                t = tfile.Torrent(data=read_url(build_opener(), torrent_url), path='/dev/null')
+                log_stdout.print("Checking %s", entry['title'])
+                if not t.get_hash() in hashes:
+                    client.load_torrent(t, dest_path)
+                    log_stdout.print("Torrent loaded to %s", dest_path)
 
 
 if __name__ == "__main__":
